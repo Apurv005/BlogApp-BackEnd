022abda9123003e9905693820cb2227b672c726d
Apurv Patel <patelapurv2001@gmail.com>
2025-04-16 11:18:42 +1200

    create post api done

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+# blog/models.py
+
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+from django.db import models
+
+# Create your models here.
---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+# blog/urls.py
+
+from django.urls import path
+from .views import create_post
+
+urlpatterns = [
+    path('create/', create_post, name='create-post'),
+]
